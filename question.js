b24580afbb39838bf9d2590f5c0aa7d001989469
Shawn T <shawn@huthunt.com>
2018-02-21 12:59:53 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,17 @@
 'use strict';
+class Question{
+  constructor(content){
+    this.content = content
+    this.save()
+  }
+  
+  save(){
+    this.constructor._All.push(this)
+  }
+  
+  static All() {
+    return this._All
+  }
+}
+
+Question._All = []
